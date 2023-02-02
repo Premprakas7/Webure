174c343967d42775689db35049f67207cf1e77b8
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-02-02 15:53:51 +0530

    tailwind added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+
+        <div >
+            <div className='flex'>
+                
+                <div><p className='text-xl font-serif'>Websure Technology</p></div>
+                <div className='flex '>
+                    <p>About Us</p>
+                    <p>Services</p>
+                    <p>Careers</p>
+                    <p>ROI stories</p>
+                    <p>Blog</p>
+                    <p>Contact Us</p>
+                    <button>Let's talk</button>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                <div className='flex '>
+                <p>Best Bussiness Platform -World Record 2022</p>
+            </div>
+            <div>
+                <p>Reach Your Bussiness Goals in Record Time</p>
+            </div>
+
+            <div>
+                <p>Support small bussiness and join the nationwide movement to encourage commercial support for millions
+                    of minority owend bussiness helping world economy
+                </p>
+            </div>
+
+            <div className='flex'>
+                <button>Gets Started</button>
+                <p>Book a 30 Min Quick Meeting</p>
+
+            </div>
+                </div>
+
+                <div>
+                    
+                </div>
+
+            </div>
+           
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
